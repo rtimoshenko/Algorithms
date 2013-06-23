8ca465fe6540c09bf70b7e5b949afc2f8a698dc6
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-06-23 16:02:46 -0500

    Initial commit.

---
+++ Sieve of Eratosthenes/main.php
@@ -0,0 +1,68 @@
+<?php
+
+/* =================================
+ *
+ * 	Ronald Timoshenko | ronaldtimoshenko.com
+ * 	2013-06-23
+ *
+ * ================================= */
+
+
+
+// Error reporting and assertion
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+assert_options(ASSERT_QUIET_EVAL, 1);
+assert_options(ASSERT_CALLBACK, 'assert_handler');
+
+function assert_handler($file, $line, $code)
+{
+	echo "Assertion failed at $file line: $line -> $code \n";
+}
+
+
+
+
+
+
+/*
+* Return an array of prime numbers between the passed $min and $max range
+*/
+function getPrimesInRange($min = 2, $max = 1000)
+{
+	assert('$min > 1  /* $min value cannot be less than 2 */');
+	assert('$max > $min  /* $min value must be greater than $max value */');
+	
+	// Implementation of Sieve of Eratosthenes
+	// http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+	
+	$primes = array();
+	
+	// Populate with all integers initially
+	for ($i = $min; $i < $max; $i++)
+		$primes[$i] = $i;
+	
+	// Enumerate every integer from the min value up to the square root of the max
+	for ($i = $min; $i < sqrt($max); $i++)
+	{
+		// Start with the square of the step, then continuosly increment the previous  
+		// value by the step size since the sequence is:
+		// n^2, n^2 + n, n^2 + 2n, ..., n
+		for ($j = $i * $i; $j < $max; $j += $i)
+		{
+			// If we were able to get the value by multiplying two numbers together,
+			// then the number is not a prime and we can remove it from the array
+			unset($primes[$j]);
+		}
+	}
+	
+	return $primes;
+}
+
+$primes = getPrimesInRange(2, 1000);
+
+var_dump($primes);
+
