07c0dee452e7a1a5ebab063b30bdd81db69d35fe
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-06-23 20:40:58 -0500

    Added conditional to prevent redundant enumeration of previously removed integers.

--- Sieve of Eratosthenes/main.php
+++ Sieve of Eratosthenes/main.php
@@ -43,26 +43,32 @@ function getPrimesInRange($min = 2, $max = 1000)
 	
 	// Populate with all integers initially
 	for ($i = $min; $i < $max; $i++)
+	{
 		$primes[$i] = $i;
-	
+	}
+		
 	// Enumerate every integer from 2 up to the square root of the max
 	for ($i = 2; $i < sqrt($max); $i++)
 	{
-		// Start with the square of the step, then continuosly increment the previous  
-		// value by the step size since the sequence is:
-		// n^2, n^2 + n, n^2 + 2n, ..., n
-		for ($j = $i * $i; $j < $max; $j += $i)
+		// If the index doesn't exist, then we've already removed it (i.e., it's not a prime)
+		// when we enumerated smaller integers and it can be skipped
+		if (isset($primes[$i]))
 		{
-			// If we were able to get the value by multiplying two numbers together,
-			// then the number is not a prime and we can remove it from the array
-			unset($primes[$j]);
+			// Start with the square of the step, then continuosly increment the previous  
+			// value by the step size since the sequence is:
+			// n^2, n^2 + n, n^2 + 2n, ..., n
+			for ($j = $i * $i; $j < $max; $j += $i)
+			{
+				// If we were able to get the value by multiplying two numbers together,
+				// then the number is not a prime and we can remove it from the array
+				unset($primes[$j]);
+			}
 		}
 	}
 	
 	return $primes;
 }
 
-$primes = getPrimesInRange(2, 1000);
-
-var_dump($primes);
+// OUTPUT
+var_dump(getPrimesInRange(2, 1000));
 
