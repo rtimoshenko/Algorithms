7f77ef245d50c7dd56629c7d8784dd68209d0b71
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-06-26 12:07:31 -0500

    Converted tabs to spaces.

--- Sieve of Eratosthenes/main.cs
+++ Sieve of Eratosthenes/main.cs
@@ -1,7 +1,7 @@
 ﻿/* =================================
  *
- * 	Ronald Timoshenko | ronaldtimoshenko.com
- * 	2013-06-25
+ *  Ronald Timoshenko | ronaldtimoshenko.com
+ *  2013-06-25
  *
  * ================================= */
 
@@ -11,55 +11,55 @@ public class Main
 {
     void Main()
     {
-	    Console.Write(GetPrimesInRange(2, 1000));
+        Console.Write(GetPrimesInRange(2, 1000));
     }
 
     IEnumerable<int> GetPrimesInRange(int min, int max)
     {
-	    // Integrity check
-	    Debug.Assert(min > 1, "min value cannot be less than 2.");
-	    Debug.Assert(max > min, "max value must be greater than min value.");
+        // Integrity check
+        Debug.Assert(min > 1, "min value cannot be less than 2.");
+        Debug.Assert(max > min, "max value must be greater than min value.");
 
-		// Implementation of Sieve of Eratosthenes
-		// http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-	
-		// Initialize nullable integer array with length of max
-	    var ints = new int?[max];
-	    int i, j;
-	
-		// Populate ints array with all integers in range, initially
-	    for (i = min; i < max; i++)
-	    {
-		    ints[i - min + 2] = i;
-	    }
-	
-		// Enumerate every integer from 2 up to the square root of the max
-	    for (i = 2; i < Math.Sqrt(max); i++)
-	    {
-			// If the index is null, then we've already marked it (i.e., it's not a prime)
-			// as non-prime when we enumerated smaller integers and it can be skipped
-		    if (ints[i] != null)
-		    {
-				// Start with the square of the step, then continuosly increment the previous  
-				// value by the step size since the sequence is:
-				// n^2, n^2 + n, n^2 + 2n, ..., n
-			    for (j = i * i; j < max; j += i)
-			    {
-					// If we were able to get the value by multiplying two numbers together,
-					// then the number is not a prime and we can set it to null
-				    ints[j] = null;
-			    }
-		    }
-	    }
-	
-	    // At this point the sieve is complete, but since we want an array result,
-	    // we enumerate the integers again and yield the primes
-	    foreach (var n in ints)
-	    {
-		    if (n != null && n > min)
-		    {
-			    yield return (int)n;
-		    }
-	    }
+        // Implementation of Sieve of Eratosthenes
+        // http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    
+        // Initialize nullable integer array with length of max
+        var ints = new int?[max];
+        int i, j;
+    
+        // Populate ints array with all integers in range, initially
+        for (i = min; i < max; i++)
+        {
+            ints[i - min + 2] = i;
+        }
+    
+        // Enumerate every integer from 2 up to the square root of the max
+        for (i = 2; i < Math.Sqrt(max); i++)
+        {
+            // If the index is null, then we've already marked it (i.e., it's not a prime)
+            // as non-prime when we enumerated smaller integers and it can be skipped
+            if (ints[i] != null)
+            {
+                // Start with the square of the step, then continuosly increment the previous  
+                // value by the step size since the sequence is:
+                // n^2, n^2 + n, n^2 + 2n, ..., n
+                for (j = i * i; j < max; j += i)
+                {
+                    // If we were able to get the value by multiplying two numbers together,
+                    // then the number is not a prime and we can set it to null
+                    ints[j] = null;
+                }
+            }
+        }
+    
+        // At this point the sieve is complete, but since we want an array result,
+        // we enumerate the integers again and yield the primes
+        foreach (var n in ints)
+        {
+            if (n != null && n > min)
+            {
+                yield return (int)n;
+            }
+        }
     }
 }
--- Sieve of Eratosthenes/main.php
+++ Sieve of Eratosthenes/main.php
@@ -2,8 +2,8 @@
 
 /* =================================
  *
- * 	Ronald Timoshenko | ronaldtimoshenko.com
- * 	2013-06-23
+ *  Ronald Timoshenko | ronaldtimoshenko.com
+ *  2013-06-23
  *
  * ================================= */
 
@@ -20,7 +20,7 @@ assert_options(ASSERT_CALLBACK, 'assert_handler');
 
 function assert_handler($file, $line, $code)
 {
-	echo "Assertion failed at $file line: $line -> $code \n";
+    echo "Assertion failed at $file line: $line -> $code \n";
 }
 
 
@@ -33,48 +33,48 @@ function assert_handler($file, $line, $code)
 */
 function getPrimesInRange($min, $max)
 {
-	// Integrity check
-	assert('$min > 1  /* $min value cannot be less than 2 */');
-	assert('$max >= $min  /* $min value must be greater than $max value */');
-	
-	// Implementation of Sieve of Eratosthenes
-	// http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-	
-	$primes = array();
-	
-	// Populate with all integers in range, initially
-	for ($i = $min; $i < $max; $i++)
-	{
-		$primes[$i - $min + 2] = $i;
-	}
-		
-	// Enumerate every integer from 2 up to the square root of the max
-	for ($i = 2; $i < sqrt($max); $i++)
-	{
-		// If the index doesn't exist, then we've already removed it (i.e., it's not a prime)
-		// when we enumerated smaller integers and it can be skipped
-		if (isset($primes[$i]))
-		{
-			// Start with the square of the step, then continuosly increment the previous  
-			// value by the step size since the sequence is:
-			// n^2, n^2 + n, n^2 + 2n, ..., n
-			for ($j = $i * $i; $j < $max; $j += $i)
-			{
-				// If we were able to get the value by multiplying two numbers together,
-				// then the number is not a prime and we can remove it from the array
-				unset($primes[$j]);
-			}
-		}
-	}
+    // Integrity check
+    assert('$min > 1  /* $min value cannot be less than 2 */');
+    assert('$max >= $min  /* $min value must be greater than $max value */');
+    
+    // Implementation of Sieve of Eratosthenes
+    // http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    
+    $primes = array();
+    
+    // Populate with all integers in range, initially
+    for ($i = $min; $i < $max; $i++)
+    {
+        $primes[$i - $min + 2] = $i;
+    }
+        
+    // Enumerate every integer from 2 up to the square root of the max
+    for ($i = 2; $i < sqrt($max); $i++)
+    {
+        // If the index doesn't exist, then we've already removed it (i.e., it's not a prime)
+        // when we enumerated smaller integers and it can be skipped
+        if (isset($primes[$i]))
+        {
+            // Start with the square of the step, then continuosly increment the previous  
+            // value by the step size since the sequence is:
+            // n^2, n^2 + n, n^2 + 2n, ..., n
+            for ($j = $i * $i; $j < $max; $j += $i)
+            {
+                // If we were able to get the value by multiplying two numbers together,
+                // then the number is not a prime and we can remove it from the array
+                unset($primes[$j]);
+            }
+        }
+    }
 
-	// Since we want all of the values within the range, we don't want
-	// to return the min value
-	if (reset($primes) == $min)
-	{
-		unset($primes[key($primes)]);
-	}
-	
-	return array_values($primes);
+    // Since we want all of the values within the range, we don't want
+    // to return the min value
+    if (reset($primes) == $min)
+    {
+        unset($primes[key($primes)]);
+    }
+    
+    return array_values($primes);
 }
 
 // OUTPUT
