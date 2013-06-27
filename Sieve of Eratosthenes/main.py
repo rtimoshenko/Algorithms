2bd731e05fef909b7db4c3bd00bb23488ecd5c41
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-06-27 15:59:36 -0500

    Added python stub for sieve.

---
+++ Sieve of Eratosthenes/main.py
@@ -0,0 +1,15 @@
+''' =================================
+ *
+ *  Ronald Timoshenko | ronaldtimoshenko.com
+ *  2013-06-27
+ *
+ * ================================= '''
+
+def main():
+    print(get_primes_in_range(2, 1000))
+
+def get_primes_in_range(range_min, range_max):
+    return None
+
+if __name__ == '__main__':
+    main()
