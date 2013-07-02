1f279f74cbf77eb408f1d78e2c9aaf334f035215
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-07-02 15:40:11 -0500

    Continued Python implementation of sieve.

--- Sieve of Eratosthenes/main.py
+++ Sieve of Eratosthenes/main.py
@@ -8,8 +8,21 @@
 def main():
     print(get_primes_in_range(2, 1000))
 
+
 def get_primes_in_range(range_min, range_max):
-    return None
+
+    # Integrity Check
+    assert range_min > 1, "range_min cannot be less than 2"
+    assert range_max > range_min, "range_max must be greater than range_min"
+
+    primes = range(range_min, range_max)
+
+    for i in range(2, math.sqrt(range_max)):
+        for j in range(i*i, range_max, i):
+            primes[j] = None
+            
+
+    return primes
 
 if __name__ == '__main__':
     main()
